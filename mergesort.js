186e00cca30363fc81786fd6b2de49eed7517b89
Amanda G <amagonzalez27@gmail.com>
2018-11-05 16:17:24 -0500

    modified split and merger func

--- mergesort.js
+++ mergesort.js
@@ -1,29 +1,32 @@
-// function split(wholeArray) {
-//   /* your code here to define the firstHalf and secondHalf arrays */
-//   const firstHalf = wholeArray.slice(0, Math.floor(wholeArray.length / 2));
-//   const secondHalf = wholeArray.slice(
-//     Math.floor(wholeArray.length / 2),
-//     wholeArray.length
-//   );
-//   let arr = [];
-//   // BASE CASE
-//   if (firstHalf.length === 1 && secondHalf.length === 1) {
-//     return arr;
-//   } else {
-//     split([firstHalf, secondHalf]);
-//   }
-//   // return [firstHalf, secondHalf];
-// }
-
 function split(wholeArray) {
-  let arr = [];
-  for (let i = 0; i < wholeArray.length; i++) {
-    arr.push([wholeArray[i]]);
-  }
-  return arr;
+  /* your code here to define the firstHalf and secondHalf arrays */
+  const middle = wholeArray / 2;
+  const firstHalf = wholeArray.slice(0, middle);
+  const secondHalf = wholeArray.slice(middle);
+
+  return merge([firstHalf, secondHalf]);
 }
 
-console.log(split([1, 2, 3, 4]));
+function merge(arr) {
+  let results = [];
+  let leftPointer = 0; // 1 2
+  let rightPointer = 0; // 0 1
+
+  // if leftPointer < arr[leftpointer]
+    whiile(leftPointer < arr[leftPointer].length && rightPointer < arr[rightPointer].length) {
+      if(arr[leftPointer] < arr[rightPointer]) {
+        results.push(arr[leftPointer])
+        leftPointer++;
+      } else {
+        results.push(arr[rightPointer]);
+        rightPointer++;
+      }
+    }
+
+
+    return results;
+  }
+
 
 function mergeSort(array) {
   /* your code here */
