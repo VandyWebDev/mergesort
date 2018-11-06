ec8406015bf63f5453ede050a0c8089755a75151
Amanda G <amagonzalez27@gmail.com>
2018-11-06 10:16:10 -0500

    solution to mergesort

--- mergesort.js
+++ mergesort.js
@@ -1,13 +1,4 @@
-function split(wholeArray) {
-  /* your code here to define the firstHalf and secondHalf arrays */
-  const middle = wholeArray / 2;
-  const firstHalf = wholeArray.slice(0, middle);
-  const secondHalf = wholeArray.slice(middle);
-
-  return merge([firstHalf, secondHalf]);
-}
-
-function merge(arr) {
+const merge = arr => {
   let results = [];
   let firstHalf = arr[0];
   let secondHalf = arr[1];
@@ -22,21 +13,28 @@ function merge(arr) {
     } else {
       results.push(secondHalf[rightPointer]);
       rightPointer++;
-
     }
   }
-  while (leftPointer < firstHalf.length) {
-    results.push(firstHalf[leftPointer]);
-    leftPointer++;
-  }
-  while (rightPointer < secondHalf.length) {
-    results.push(secondHalf[rightPointer]);
-    rightPointer++;
-  }
+  for (; leftPointer < firstHalf.length; leftPointer++)
+    results.push(firstHalf(leftPointer));
+  for (; rightPointer < secondHalf.length; rightPointer++)
+    results.push(secondHalf(rightPointer));
   return results;
-}
+};
 
+const split = wholeArray => {
+  /* your code here to define the firstHalf and secondHalf arrays */
+  const middle = wholeArray / 2;
+  const firstHalf = wholeArray.slice(0, middle);
+  const secondHalf = wholeArray.slice(middle);
 
-function mergeSort(array) {
-  /* your code here */
-}
+  return merge([firstHalf, secondHalf]);
+};
+
+const mergeSort = array => {
+  if (array.lenght < 2) return array; // base case
+  const splits = split(array),
+    left = splits[0],
+    right = splits[1];
+  return merge(mergeSort(left), mergeSort(right));
+};
